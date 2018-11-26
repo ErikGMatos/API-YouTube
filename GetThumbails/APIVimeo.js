11b7a834febfa4bdb918bf1865863c1c23e2a031
Erik Matos <erik_senac@hotmail.com>
2018-11-26 12:35:46 -0200

    Adicionando GetThumbnails

---
+++ GetThumbails/APIVimeo.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var APIYT = function () {
+
+    function init() {
+        bindFunctions();
+
+    }
+
+    function bindFunctions() {
+
+        $('.botao.vimeo').click(function () {
+            var endpoint = 'https://vimeo.com/api/oembed.json';
+            var url = endpoint + '?url=' + encodeURIComponent('http://www.vimeo.com/7100569');
+            new GCS().setObj({
+                type: 'GET',
+                processData: true,
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                url: url,
+                success: function (data) {
+                    //debugger;
+                    console.log(data)
+                    var html = '<img width="480" src="'+data.thumbnail_url+'">';
+                    
+                    $('.row').html(html);
+                }
+            }).executar();
+        });
+
+    }
+
+
+    return {
+        init: init
+    };
+
+}();
+$(APIYT.init);
---
+++ GetThumbails/APIYouTubeV3.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var APIYT = function () {
+
+    function init() {
+        bindFunctions();
+    }
+
+    function bindFunctions() {
+
+        $('.botao.youtube').click(function () {
+            new GCS().setObj({
+                type: 'GET',
+                processData: true,
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                url: 'https://www.googleapis.com/youtube/v3/videos',
+                data: {
+                    part: 'snippet',
+                    key: 'AIzaSyDhtU9oebZG4uIMA63utC6AT0OYrXQnpzs',
+                    id: 'lKM-8CZRplI'
+                },
+                success: function (data) {
+                    console.log(data.items)
+                    var item = data.items;
+                    var html = '';
+                    for (var i = 0; i < item.length; i++) {
+                        html += `<img width="480" src="${item[i].snippet.thumbnails.medium.url}">`
+                    }
+                   
+                    $('.row').html(html);
+                }
+            }).executar();
+        });
+    }
+
+    return {
+        init: init
+    };
+
+}();
+$(APIYT.init);
